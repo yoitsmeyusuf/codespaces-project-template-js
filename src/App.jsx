bcc737cafb6934ace38313fcdc13903569edff1e
Yusuf Kaydın <95529238+yoitsmeyusuf@users.noreply.github.com>
2023-09-09 16:03:05 +0000

    okeey

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Yusuf Kaydın",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  email: "Yusuf@example.com",
+  gitHub: "yoitsmeyusuf",
+  instagram: "yoitsmeyusuf",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
+  twitter: "yoitsmeyusuf",
   youTube: "Code",
 };
 
